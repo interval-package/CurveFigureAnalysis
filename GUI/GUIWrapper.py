babfc4b046c2ce20e9b1622c68929ef34d9f21fa
Zza <1519162203@qq.com>
2022-04-19 13:32:11 +0800

    start to create gui

--- GUI/GUIWrapper.py
+++ GUI/GUIWrapper.py
@@ -2,6 +2,8 @@ from PyQt5.QtWidgets import QStackedWidget
 
 from GUI.Pages.MainWindow.MainWindow import *
 
+from GUI.Pages.StartPage.StartPageWrapper import StartPage
+
 
 class GUIMain(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -11,3 +13,21 @@ class GUIMain(QtWidgets.QMainWindow, Ui_MainWindow):
         # 设置页面
         self.stackWidget = QStackedWidget()
 
+        # 0
+        self.StartPage = StartPage()
+        self.stackWidget.addWidget(self.StartPage)
+
+        # 将页面置放于窗体中间
+        self.setCentralWidget(self.stackWidget)
+        self.switchPage(0)
+
+        self.setIcon()
+
+    def switchPage(self, index):
+        self.stackWidget.setCurrentIndex(index)
+
+    def setIcon(self):
+        # 由于路径问题需要重新设置一下icon的路径
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("GUI/Assets/icon_music.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setWindowIcon(icon)
---
+++ GUI/Pages/StartPage/StartPageWrapper.py
@@ -0,0 +1,7 @@
+from GUI.Pages.StartPage.StartPage import *
+
+
+class StartPage(QtWidgets.QWidget, Ui_StartPage):
+    def __init__(self):
+        super(StartPage, self).__init__()
+        self.setupUi(self)
